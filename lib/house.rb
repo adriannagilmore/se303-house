c23632c385a495a9019b36da199d0c17c40c19e6
Adrianna Gilmore <a_inman33@yahoo.com>
2021-04-15 10:32:06 -0700

    added case for verse 2

--- lib/house.rb
+++ lib/house.rb
@@ -3,5 +3,8 @@ class House
         case lineNum
         when 1
             "This is the house that Jack built.\n" 
+        when 2
+            "This is the malt that lay in the house that Jack built.\n"
+        end
     end
 end
