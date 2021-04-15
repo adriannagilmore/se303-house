01643cdef7ecae59069d33f4557df41277850d42
Adrianna Gilmore <a_inman33@yahoo.com>
2021-04-15 10:49:46 -0700

    added case 0 for all verses and added recite function to call line(0)

--- lib/house.rb
+++ lib/house.rb
@@ -1,11 +1,14 @@
 class House
+    def recite
+        line(0)
+    end
+
     def line(lineNum)
         case lineNum
         when 1
             "This is the house that Jack built.\n" 
         when 2
             "This is the malt that lay in the house that Jack built.\n"
-        
         when 3
             "This is the rat that ate the malt that lay in the house that Jack built.\n"
         when 4
@@ -26,6 +29,30 @@ class House
             "This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         when 12
             "This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+        when 0
+            "This is the house that Jack built.\n\n
+
+            This is the malt that lay in the house that Jack built.\n\n
+            
+            This is the rat that ate the malt that lay in the house that Jack built.\n\n
+            
+            This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n\n
+            
+            This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n\n
+            
+            This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n\n
+            
+            This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n\n
+            
+            This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n\n
+            
+            This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n\n
+            
+            This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n\n
+            
+            This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n\n
+            
+            This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n\n"
         end
     end
 end
