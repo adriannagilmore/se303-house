1a2018ac77fa96ddebb3b4ad25064d959770282e
Adrianna Gilmore <a_inman33@yahoo.com>
2021-05-25 10:41:26 -0700

    added new pirate test

--- test/house_test.rb
+++ test/house_test.rb
@@ -99,4 +99,9 @@ This is the horse and the hound and the horn that belonged to the farmer sowing 
     assert_equal expected, House.new("Thar be").line(1)
   end
 
+  def pirate_test_line_2
+    expected = "Thar be the malt that lay in the house that Jack built.\n"
+    assert_equal expected, House.new("Thar be").line(2)
+  end
+
 end
