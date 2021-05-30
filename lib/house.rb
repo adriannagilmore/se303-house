4af48b93267e9fbe615ab6bc2542b68d5fc9ad59
Adrianna Gilmore <a_inman33@yahoo.com>
2021-05-29 21:09:33 -0700

    added new class to handle subject and verb of verses

--- lib/house.rb
+++ lib/house.rb
@@ -5,52 +5,12 @@ class House
         @line_start = line_start
     end
 
-    def phrase
-    [
-        "", 
-
-        " the house that Jack built",
-  
-        " the malt that lay in",
-  
-        " the rat that ate",
-  
-        " the cat that killed",
-  
-        " the dog that worried",
-  
-        " the cow with the crumpled horn that tossed",
-  
-        " the maiden all forlorn that milked",
-  
-        " the man all tattered and torn that kissed",
-  
-        " the priest all shaven and shorn that married",
-  
-        " the rooster that crowed in the morn that woke",
-  
-        " the farmer sowing his corn that kept",
-  
-        " the horse and the hound and the horn that belonged to"
-    ]
-    end
-
     def recite
-        (1..phrase.length-1).map { |lineNum| line(lineNum) }.join("\n")
-    end
-
-    def add_phrases(lineNum)
-      full_phrase = ""
-      
-      for i in 1..lineNum do
-        full_phrase = phrase[i] + full_phrase
-      end
-      
-      full_phrase
+        (1..12).map { |lineNum| line(lineNum) }.join("\n")
     end
 
     def line(lineNum)
-      "#{line_start}#{add_phrases(lineNum)}.\n"
+      "#{line_start}#{SubjectAndVerb.new.add_phrases(lineNum)}.\n"
     end
 end
 
@@ -104,14 +64,88 @@ class RandomWithJackEnding < RandomVersion
   end
 end
 
-puts RandomVersion.new.recite
+class SubjectAndVerb < House
+  def subject
+    [
+      "", 
+    
+      " the house",
+    
+      " the malt",
+    
+      " the rat",
+    
+      " the cat",
+    
+      " the dog",
+    
+      " the cow with the crumpled horn",
+    
+      " the maiden all forlorn",
+    
+      " the man all tattered and torn",
+    
+      " the priest all shaven and shorn",
+    
+      " the rooster that crowed in the morn",
+    
+      " the farmer sowing his corn",
+    
+      " the horse and the hound and the horn",
+    ]
+  end
 
-puts ""
-puts ""
+  def verb
+    [
+      "", 
+    
+      " that Jack built",
+    
+      " that lay in",
+    
+      " that ate",
+    
+      " that killed",
+    
+      " that worried",
+    
+      " that tossed",
 
-puts RandomVersion.new("Thar be").recite
+      " that milked",
+    
+      " that kissed",
+    
+      " that married",
+    
+      " that woke",
+    
+      " that kept",
+    
+      " that belonged to",
+    ]
+  end
 
-puts ""
-puts ""
+  def add_phrases(lineNum)
+    full_phrase = ""
+    
+    for i in 0..lineNum do
+      full_phrase = subject[i] + verb[i] + full_phrase
+    end
+    
+    full_phrase
+  end
+end
 
-puts RandomWithJackEnding.new("Thar be").recite
+#puts RandomVersion.new.recite
+
+#puts ""
+#puts ""
+
+#puts RandomVersion.new("Thar be").recite
+
+#puts ""
+#puts ""
+
+#puts RandomWithJackEnding.new("Thar be").recite
+
+#puts ChangeSubjectAndVerb.new.recite
